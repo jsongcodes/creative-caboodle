e718e5562789d32c2e3c061ebdd3faa723f5cc34
Jasmin <ssongnari@gmail.com>
2023-03-24 12:41:44 -0400

    create useEffect and login/logout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,32 @@
 import "../App.css";
+import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import TopicList from "./TopicList";
 import Login from "./Login";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    fetch('/me').then((res) => {
+      if (res.ok){
+        res.json().then((user) => {
+          setIsAuthenticated(true);
+          setUser({
+            id: user.id,
+            username: user.username
+          });
+        });
+      }
+    });
+  }, [])
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
   return (
     <>
       <div>App component</div>
