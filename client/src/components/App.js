f5cff77b9cc091a5a02760ad3bc8bd4a214a2e79
Jasmin <ssongnari@gmail.com>
2023-03-24 14:09:13 -0400

    change hierarchy

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,36 +5,54 @@ import Navbar from "./Navbar";
 import TopicList from "./TopicList";
 import Login from "./Login";
 
+import TopicForm from "./TopicForm";
+import NoteList from "./NoteList";
+import Topic from "./Topic";
+
 const App = () => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    fetch('/me').then((res) => {
-      if (res.ok){
+    fetch("/me").then((res) => {
+      if (res.ok) {
         res.json().then((user) => {
           setIsAuthenticated(true);
           setUser({
             id: user.id,
-            username: user.username
+            username: user.username,
           });
         });
       }
     });
-  }, [])
+  }, []);
 
   const handleLogout = () => {
     setUser(null);
   };
 
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
     <>
       <div>App component</div>
-      <Navbar/>
-      <TopicList />
-      <Login/>
+      <Navbar user={user} setUser={setUser} handleLogout={handleLogout} />
+      <Switch>
+        <Route exact path="/">
+          <TopicList />
+        </Route>
+        <Route exact path="/newtopic">
+          <TopicForm />
+        </Route>
+        <Route exact path="/notes">
+          <NoteList />
+        </Route>
+        <Route exact path="/topics/:id">
+          <Topic />
+        </Route>
+      </Switch>
     </>
   );
-}
+};
 
 export default App;
