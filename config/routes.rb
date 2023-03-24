0486eb9b9bc38e4189161db11da9e7a1ae54738d
Jasmin <ssongnari@gmail.com>
2023-03-24 12:39:54 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  # # Routing logic: fallback requests for React Router.
+  # # Leave this here to help deploy your app later!
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  resources :notes
+  resources :topics, only: [:index, :show, :create]
+  resources :resources, only: [:index, :show, :create]
+  resources :users, onlly: [:index]
   
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
 end
