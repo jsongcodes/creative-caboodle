7a0abe417ea0051c38b03dc3902542d2a55259d3
Jasmin <ssongnari@gmail.com>
2023-04-07 07:12:00 -0400

    testing

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -7,6 +7,9 @@ const systemMessage = {
     "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
   }
 
+
+  //hi
+
 const API_KEY = process.env.REACT_APP_OPENAI_API_KEY
 
   function Chat() {
